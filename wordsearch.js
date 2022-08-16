9ee3c28be385990ac62d93ca356b200dc6fc2a2c
Janet <Janet_Lam@live.ca>
2022-08-15 22:54:48 -0400

    Code and tests complete with stretch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    const reversedHorizontalJoin = horizontalJoin.map(s => s.split('').reverse().join(''));
+    const diagonllyJoin = letters.map((ls, index) => ls[index]).join('');
+  
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) return true;
     }
-}
+  
+    for (const l of verticalJoin) {
+      if (l.includes(word)) return true;
+    } 
+  
+    for (const l of reversedHorizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    if (diagonllyJoin.includes(word)) return true;
+  
+    return false;
+  };
+  
+  const transpose = function(matrix) {
+    const transposed = [];
+    // c: column
+    for (let c = 0; c < matrix[0].length; c++) {
+      transposed.push([]);
+      // r: row
+      for (let r = 0; r < matrix.length; r++) {
+        transposed[c].push(matrix[r][c]);
+      }
+    }
+  
+    return transposed;
+  };
 
-module.exports = wordSearch
+    module.exports = wordSearch;
+
+// Pair Programming with Atsuyuki Yoshimatsu
